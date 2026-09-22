aa5d75c12ff98b2b1bfb9f8eefc7abf7ea9506a2
wgravinajunior-design <wgravinajunior@gmail.com>
2026-09-22 18:39:25 -0300

    v1.0.1: Correção leitura de etiquetas

--- lib/config/app_version.dart
+++ lib/config/app_version.dart
@@ -1,11 +1,11 @@
 /// Versão e configurações do repositório para verificação de atualização.
 class AppVersion {
-  static const String version = '1.0.0';
-  static const int buildNumber = 3;
+  static const String version = '1.0.1';
+  static const int buildNumber = 4;
 
   static const String githubOwner = 'wgravinajunior-design';
   static const String githubRepo = 'buscapreco';
 
-  static String get display => 'v1.0.0';
+  static String get display => 'v1.0.1';
 }
 
